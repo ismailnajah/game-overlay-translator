bf9fd39fa46b99728f2c9b03f92ea98fa67a59e7
ismailnajah <najah.ismail.etu@gmail.com>
2023-11-10 14:17:47 +0100

    added multi-threading to handle translation

--- translator.py
+++ translator.py
@@ -1,13 +1,20 @@
+from threading import Thread, Lock
 from googletrans import Translator
 import matplotlib.pyplot as plt
+import cv2
 import numpy as np
 from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image
 import mss
 import pytesseract
+import time
+import random
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
+translator = Translator()
+translated_text = []
+share_lock = Lock()
 
 def threshold(img):
     # preprocess
@@ -18,14 +25,56 @@ def threshold(img):
     return img
 
 
+def remove_noise(image):
+    return cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 15)
+
+def to_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 200, cv2.THRESH_OTSU)[1]
+
+def sharpen(image):
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5, -1],
+                       [0, -1, 0]])
+    sharpened = cv2.filter2D(image, -1, kernel)
+    return sharpened
+
+def preprocess(img):
+    #img = remove_noise(img)
+    #img = to_grayscale(img)
+    img = sharpen(img)
+    return img
+
+def OCR_translate(capture_box):
+    print("thread started ", capture_box)
+    scr = mss.mss()
+    while True:
+        img = np.array(scr.grab(capture_box))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img - preprocess(img)
+        # # setting the OCR language target to turkish
+        custom_config = r'-l tur --psm 6'
+        original_text = pytesseract.image_to_string(img, config=custom_config)
+        translation = translator.translate(original_text)
+        #TODO: Cleanup the text
+        with share_lock:
+                translated_text.append(translation.text)
+        #time.sleep(0.05)
+    
+
+
 class MainWindow:
     def __init__(self, w, h, title='main window') -> None:
         self.w = w
         self.h = h
         self.root = Tk()
         self.grid_window = None
-        self.scr = mss.mss()
-        self.translator = Translator()
+        self.thread_started = False
+
+        #debugging
+        self.text = None
 
 
     def start(self):
@@ -34,10 +83,17 @@ class MainWindow:
         self.root.configure(bg='lightgray')
 
         start_grid_btn = Button(self.root, text = 'Select Area To Translate', bd = '3', command = self.start_grid)
-        start_grid_btn.pack(side='top', pady=5) 
-        self.text_field = Text(self.root, height = 100, width = 100)
-        self.text_field.configure(font=('Helvetica bold',8))
-        self.text_field.pack(side='bottom')
+        start_grid_btn.pack(side='top', pady=5)
+        
+        # TODO: Change the text field to text label
+        # self.text_field = Text(self.root, height = 100, width = 100)
+        # self.text_field.configure(font=('Helvetica bold',8))
+        self.text_field = Label(self.root, text="Translating....", 
+                                font= ('Arial 9'), background='lightgray', 
+                                justify='left', wraplength=300)
+
+        self.text_field.pack(pady=10, padx=10, side=TOP, anchor='w')
+
         self.root.mainloop()
 
     def display_image(self, event=None):
@@ -45,35 +101,43 @@ class MainWindow:
         img = Image.frombytes("RGB", img.size, img.bgra, "raw", "BGRX")
         img.save('image.png')
         img = ImageTk.PhotoImage(img)
-        print(img)
         label = Label(self.root, image = img)
         label.pack(pady=10, padx=10, side='bottom')
 
-    def translate(self, old=None):
-        img = self.scr.grab(self.capture_box)
-        img = Image.frombytes("RGB", img.size, img.bgra, "raw", "BGRX")
-        if old is None or (list(threshold(img).getdata()) != list(threshold(old).getdata())):
-            # current = np.array(img)
-            pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-            original_text = pytesseract.image_to_string(img)
-            translation = self.translator.translate(original_text)
-            self.text_field.insert(tk.END, translation.text)
-            self.text_field.see('end')
-        self.root.after(100, self.translate, img)
+
+
+    def update_text(self, text):
+        self.text_field.configure(text=text)
+
+    def translate(self):
+        if not self.thread_started:
+            self.translation_thread = Thread(target=OCR_translate, args=(self.capture_box,), daemon=True)
+            self.thread_started = True
+            self.translation_thread.start()
+
+        with share_lock:
+            if translated_text:
+                self.text = translated_text.pop(0)    
+
+        self.update_text(self.text) 
+        self.root.after(100, self.translate)
 
 
     def start_grid(self):
+        if self.grid_window != None: 
+            return
+        
         self.grid_window = Toplevel(self.root)
         self.grid_window.overrideredirect(True)
         self.root.attributes('-topmost',True)
         self.grid_window.state('zoomed')
 
-        self.grid_window.configure(bg='white')
+        self.grid_window.configure(bg='black')
         self.grid_window.attributes('-alpha', 0.3)
 
         w = self.grid_window.winfo_width()
         h = self.grid_window.winfo_height()
-        self.c = Canvas(self.grid_window, height= h, width= w, bg='white')
+        self.c = Canvas(self.grid_window, height= h, width= w, bg='lightgray')
         self.c.pack(fill=tk.BOTH, expand=True)
         self.c.bind('<Configure>',       self.draw_lines)
         self.c.bind('<Button-1>',        self.get_mouse_pos)
@@ -112,6 +176,7 @@ class MainWindow:
     def get_capture_box(self, event=None):
         self.capture_box = (self.initX, self.initY, event.x, event.y)
         self.grid_window.destroy()
+        self.grid_window = None
         self.translate()
 
     def close_grid(self):
